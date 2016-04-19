9ca609e963f48005dfe44f980f471eb6a53b8fdf
Aaron Robb <airrobb@gmail.com>
2016-04-18 23:45:36 -0500

    babel publish set up

---
+++ src/components/DynamicForm.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+//Landing Page Junk Stuff
+export default class DynamicForm extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <h1>Hi</h1>
+    )
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DynamicForm from './components/DynamicForm'
+
+ReactDOM.render(<DynamicForm/>, document.getElementById('app'))
