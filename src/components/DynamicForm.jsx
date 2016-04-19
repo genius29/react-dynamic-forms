938082ad777443e0ac68c9fa7ea2761808792426
Aaron Robb <airrobb@gmail.com>
2016-04-19 00:12:37 -0500

    default usage works

--- src/components/DynamicForm.jsx
+++ src/components/DynamicForm.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
-
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 
 //Landing Page Junk Stuff
 export default class DynamicForm extends Component {
   constructor(props) {
     super(props)
   }
-
+  handleSubmit(e) {
+    e.preventDefault()
+    const formData = {}
+    this.props.inputs.map((input) => {
+      formData[input.name] = this.refs[input.name].value
+      this.refs[input.name].value = ''
+    })
+    this.props.callBack(formData)
+  }
   render() {
-
+    const { inputs } = this.props
+    const fields = inputs.map((input, idx) => {
+      return <div className="form-group"  key={idx}>
+        <label>{input.name}</label>
+        <input className="form-control" name={input.name} ref={input.name} type={input.type}/>
+      </div>
+    })
     return (
-      <h1>Hi</h1>
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        { fields }
+        <button className="btn btn-success" type="submit">Submit</button>
+      </form>
     )
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import DynamicForm from './components/DynamicForm'
 
-ReactDOM.render(<DynamicForm/>, document.getElementById('app'))
+const inputs = [{
+  name: 'name',
+  type: 'text'
+},
+{
+  name: 'email',
+  type: 'email'
+}
+]
+
+function callBack(formData) {
+  console.log(formData)
+}
+
+ReactDOM.render(<DynamicForm inputs={inputs} callBack={callBack}/>, document.getElementById('app'))
